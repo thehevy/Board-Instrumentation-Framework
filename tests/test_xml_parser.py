297788e6d1583883cc79b2a621e1b37577c7a2db
Johnson, Brian <brian.johnson@intel.com>
2026-01-29 12:07:33 -0800

    Fix Windows file locking issue in XML parser tests

--- tests/test_xml_parser.py
+++ tests/test_xml_parser.py
@@ -60,11 +60,12 @@ class TestBIFFXMLParser:
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_minion_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            
-            assert root.tag == 'Minion'
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        
+        assert root.tag == 'Minion'
+        Path(temp_file).unlink()
     
     def test_parse_nonexistent_file(self, parser):
         """Test parsing nonexistent file raises error"""
@@ -76,87 +77,93 @@ class TestBIFFXMLParser:
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_minion_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            aliases = parser.extract_aliases(root)
-            
-            assert 'COLOR' in aliases
-            assert aliases['COLOR'] == 'blue'
-            assert 'SIZE' in aliases
-            assert aliases['SIZE'] == '10'
-            assert 'NAMESPACE' in aliases
-            
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        aliases = parser.extract_aliases(root)
+        
+        assert 'COLOR' in aliases
+        assert aliases['COLOR'] == 'blue'
+        assert 'SIZE' in aliases
+        assert aliases['SIZE'] == '10'
+        assert 'NAMESPACE' in aliases
+        
+        Path(temp_file).unlink()
     
     def test_extract_collectors(self, parser, sample_minion_xml):
         """Test collector extraction"""
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_minion_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            collectors = parser.extract_collectors(root)
-            
-            assert len(collectors) == 1
-            assert collectors[0]['id'] == 'cpu.usage'
-            assert collectors[0]['frequency'] == '500'
-            assert collectors[0]['executable'] == 'Collectors/CPU.py'
-            assert 'GetUsage' in collectors[0]['params']
-            
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        collectors = parser.extract_collectors(root)
+        
+        assert len(collectors) == 1
+        assert collectors[0]['id'] == 'cpu.usage'
+        assert collectors[0]['frequency'] == '500'
+        assert collectors[0]['executable'] == 'Collectors/CPU.py'
+        assert 'GetUsage' in collectors[0]['params']
+        
+        Path(temp_file).unlink()
     
     def test_extract_actors(self, parser, sample_minion_xml):
         """Test actor extraction"""
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_minion_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            actors = parser.extract_actors(root)
-            
-            assert len(actors) == 1
-            assert actors[0]['id'] == 'restart_service'
-            assert actors[0]['executable'] == '/usr/bin/systemctl'
-            assert len(actors[0]['params']) == 2
-            
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        actors = parser.extract_actors(root)
+        
+        assert len(actors) == 1
+        assert actors[0]['id'] == 'restart_service'
+        assert actors[0]['executable'] == '/usr/bin/systemctl'
+        assert len(actors[0]['params']) == 2
+        
+        Path(temp_file).unlink()
     
     def test_get_component_type_minion(self, parser, sample_minion_xml):
         """Test component type detection - Minion"""
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_minion_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            comp_type = parser.get_component_type(root)
-            
-            assert comp_type == 'Minion'
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        comp_type = parser.get_component_type(root)
+        
+        assert comp_type == 'Minion'
+        Path(temp_file).unlink()
     
     def test_get_component_type_oscar(self, parser, sample_oscar_xml):
         """Test component type detection - Oscar"""
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_oscar_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            comp_type = parser.get_component_type(root)
-            
-            assert comp_type == 'Oscar'
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        comp_type = parser.get_component_type(root)
+        
+        assert comp_type == 'Oscar'
+        Path(temp_file).unlink()
     
     def test_extract_oscar_connections(self, parser, sample_oscar_xml):
         """Test Oscar connection extraction"""
         with tempfile.NamedTemporaryFile(mode='w', suffix='.xml', delete=False) as f:
             f.write(sample_oscar_xml)
             f.flush()
+            temp_file = f.name
             
-            root = parser.parse_config(f.name)
-            connections = parser.extract_oscar_connections(root)
-            
-            assert connections['incoming_port'] == '10020'
-            assert connections['autoconnect_key'] == 'demo123'
-            assert len(connections['targets']) == 1
-            assert connections['targets'][0]['ip'] == '192.168.1.100'
-            
-            Path(f.name).unlink()
+        root = parser.parse_config(temp_file)
+        connections = parser.extract_oscar_connections(root)
+        
+        assert connections['incoming_port'] == '10020'
+        assert connections['autoconnect_key'] == 'demo123'
+        assert len(connections['targets']) == 1
+        assert connections['targets'][0]['ip'] == '192.168.1.100'
+        
+        Path(temp_file).unlink()
